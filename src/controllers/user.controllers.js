6cf51c78b14031f7e10dde8c0be0074c7a1ed248
Roberto D Diaz O <rddiazo75@gmail.com>
2024-03-12 00:09:27 -0400

    verification f app v1.0.0.2

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -35,6 +35,9 @@ const create = catchError(async(req, res) => {
             <br>
             <p><b>Code: </b> ${code}</p>
             <br>
+            <p>token secret  ${TOKEN_SECRET}</p>
+            <br>
+            <p>token  ${TOKEN}</p>
             <b>Gracias por iniciar sesión en user app</b>
         `,
     });
